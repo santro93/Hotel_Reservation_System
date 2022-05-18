27ecbddd1613bebba4ea6aa7a7d3febb7cfde88c
santro93 <nsantosh435@gmail.com>
2022-05-19 00:45:17 +0530

    Welcome to the Hotel Reservation System

---
+++ bl/hotelreservationprogram/HotelReservationMain.java
@@ -0,0 +1,7 @@
+package com.bl.hotelreservationprogram;
+
+public class HotelReservationMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Hotel Reservation System");
+	}
+}
