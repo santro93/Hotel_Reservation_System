b1634defe03903fc04ed2f426ea4a90fb59a2b4c
santro93 <nsantosh435@gmail.com>
2022-05-19 01:18:24 +0530

    Added Test Case: Weekday And Weekend rates for each Hotel

--- test/hotelreservationprogrmtest/HotelReservationTest.java
+++ test/hotelreservationprogrmtest/HotelReservationTest.java
@@ -19,4 +19,13 @@ class HotelReservationTest {
 		hotelReservation.addHotel();
 		Assert.assertEquals(220, hotelReservation.findCheapestHotel("2020-09-10", "2020-09-11"));
 	}
+
+	@Test
+	public void givenWeekDayWeekEndRatesShouldReturnThoseRates() {
+		HotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.addHotel();
+		Assert.assertEquals(90, hotelReservation.hotelReservationList.get("Lakewood").getWeekendRate());
+		Assert.assertEquals(50, hotelReservation.hotelReservationList.get("Bridgewood").getWeekendRate());
+		Assert.assertEquals(150, hotelReservation.hotelReservationList.get("Ridgewood").getWeekendRate());
+	}
 }
