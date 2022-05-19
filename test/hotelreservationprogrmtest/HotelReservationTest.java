f04ab390aac095176dcbe9fa09e21bab3d725e5e
santro93 <nsantosh435@gmail.com>
2022-05-19 17:18:34 +0530

    Test Case: To find Cheapest Best Hotel for Regular Customer

--- test/hotelreservationprogrmtest/HotelReservationTest.java
+++ test/hotelreservationprogrmtest/HotelReservationTest.java
@@ -70,4 +70,11 @@ class HotelReservationTest {
 		obj.addHotel();
 		Assert.assertTrue(obj.isDateValid("2020-09-11", "2020-09-12"));
 	}
+	
+	@Test
+    public void givenDateRangeShouldReturnTheCheapestBestRatedHotelforRegularCustomer() {
+        HotelReservation obj = new HotelReservation();
+        obj.addHotel();
+        Assert.assertEquals(200, obj.findCheapestHotel("2020-09-11", "2020-09-12"));
+    }
 }
