6edf67199ec47ef5c03f17409ee18b3413867336
santro93 <nsantosh435@gmail.com>
2022-05-19 17:03:19 +0530

    Added : To find Cheapest Best Hotel for Regular Customer

--- bl/hotelreservationprogram/HotelReservationMain.java
+++ bl/hotelreservationprogram/HotelReservationMain.java
@@ -7,7 +7,7 @@ public class HotelReservationMain {
 		HotelReservation hotelReservation = new HotelReservation();
 		System.out.println("\nHotel List for Customer is :-");
 		hotelReservation.addHotel();
-		System.out.println("\nCheapest Best Rated Hotel for a given Date Range " + "Reward Customer is :-");
-		hotelReservation.findCheapestHotelForRewardCustomer("2020-09-11", "2020-09-12");
+		System.out.println("\nCheapest Best Rated Hotel for Regular Customer on given Date Range is :-");
+		hotelReservation.findCheapestHotel("2020-09-11", "2020-09-12");
 	}
 }
